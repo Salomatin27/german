66f195c48d945b63f0a2efe37b3acb75d80aabb8
Dron <salomatin@hotmail.com>
2020-12-08 14:57:12 +0200

    печатная форма упрощенная

--- src/Patient/src/Service/PatientService.php
+++ src/Patient/src/Service/PatientService.php
@@ -722,7 +722,7 @@ class PatientService
         $patient = $this->getPatient($id);
 
         // patient-1
-        $file = './public/doc/Patient_data.htm';
+        $file = './data/Patient_data.htm';
         $contents="";
         if ($handle = fopen($file, 'r')) {
             $contents = fread($handle, filesize($file));
